2f6c41b62340a2b35db76823426e2d94db0c4360
Patrick Driscoll <pdriscoll@satx.rr.com>
2023-09-14 16:22:16 -0500

    Update queries

--- database/queries.sql
+++ database/queries.sql
@@ -1,3 +1,4 @@
+
 #Get time of last update
 ##############################
 SELECT UPDATE_TIME
@@ -5,16 +6,31 @@ FROM information_schema.tables
 WHERE  TABLE_SCHEMA = 'passport_referrals'
 AND TABLE_NAME = 'pages'  
 
-#Get last day inserted
+#Get last day that was inserted
 ##############################
 SELECT date 
 FROM pages 
 WHERE id = (SELECT MAX(id) FROM pages);
 
+#Daily referrals 
+##############################
+SELECT
+  pages.date AS `Date`,
+  SUM(pages.views) AS `Pageviews`,
+  SUM(pages.users) AS `Users`,
+  SUM(pages.duration) AS `Duration`
+FROM pages
+WHERE pages.date >= '2023-07-01'
+AND pages.date <= '2023-08-30'
+GROUP BY `Date`
+ORDER BY `Date`;
+
 #Top shows 
 ##############################
 SELECT
+  shows.id As `ID`,
   shows.title AS `Show`,
+  shows.slug AS `Slug`,
   shows.genre AS `Genre`,
   SUM(pages.views) AS `Pageviews`,
   SUM(pages.users) AS `Users`,
@@ -24,15 +40,18 @@ INNER JOIN videos ON pages.videos_id  = videos.id
 INNER JOIN shows ON videos.shows_id = shows.id
 WHERE pages.date >= '2023-07-01'
 AND pages.date <= '2023-08-30'
-GROUP BY `Show`
+GROUP BY `ID`
 ORDER BY `Pageviews` DESC, `Users` DESC, `Show`;
 
 #Top episodes 
 ##############################
 SELECT
+  shows.id As `ShowID`,
   shows.title AS `Show`,
   shows.genre AS `Genre`,  
+  videos.id AS `VideoID`,
   videos.title AS `Episode`,
+  videos.slug AS `Slug`,
   SUM(pages.views) AS `Pageviews`,
   SUM(pages.users) AS `Users`,
   SUM(pages.duration) AS `Duration`
@@ -41,5 +60,43 @@ INNER JOIN videos ON pages.videos_id  = videos.id
 INNER JOIN shows ON videos.shows_id = shows.id
 WHERE pages.date >= '2023-07-01'
 AND pages.date <= '2023-08-30'
-GROUP BY `Show`, `Episode`
+GROUP BY `ShowID`, `VideoID`
 ORDER BY `Pageviews` DESC, `Users` DESC, `Show`, `Episode`;
+
+#Shows trends 
+##############################
+SELECT
+  pages.date AS `Date`,
+  shows.id AS `ID`,
+  shows.title AS `Show`,
+  SUM(pages.views) AS `Pageviews`,
+  SUM(pages.users) AS `Users`,
+  SUM(pages.duration) AS `Duration`
+FROM pages
+INNER JOIN videos ON pages.videos_id  = videos.id
+INNER JOIN shows ON videos.shows_id = shows.id
+WHERE pages.date >= '2023-07-01'
+AND pages.date <= '2023-08-30'
+AND shows.id IN (55, 82, 3)
+GROUP BY `Date`, `ID`
+ORDER BY `Date`, `ID`;
+
+#Episodes trends 
+##############################
+SELECT
+  pages.date AS `Date`,
+  shows.id AS `ShowID`,
+  shows.title AS `Show`,
+  videos.id AS `VideoID`,
+  videos.title AS `Episode`,  
+  SUM(pages.views) AS `Pageviews`,
+  SUM(pages.users) AS `Users`,
+  SUM(pages.duration) AS `Duration`
+FROM pages
+INNER JOIN videos ON pages.videos_id  = videos.id
+INNER JOIN shows ON videos.shows_id = shows.id
+WHERE pages.date >= '2023-07-01'
+AND pages.date <= '2023-08-30'
+AND videos.id IN (399, 220, 542)
+GROUP BY `Date`, `VideoID`  
+ORDER BY `Date`, `VideoID`;
